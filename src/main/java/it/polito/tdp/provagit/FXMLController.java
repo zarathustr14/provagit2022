28a17ee50509ca00b850f1f598ad996c505b6347
Isa <Isa@DESKTOP-ER27B2G>
2022-03-22 17:20:06 +0100

    Tradotto in italiano

--- src/main/java/it/polito/tdp/provagit/FXMLController.java
+++ src/main/java/it/polito/tdp/provagit/FXMLController.java
@@ -15,7 +15,7 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        label.setText("Buongiorno a tutti/e!");
     }
     
     @Override
